f7e429651d3423073a06aa072ce57fdf77894132
akshay8281 <akshaypitroda4@gmail.com>
2023-08-20 20:26:59 +0530

    C_Projects_files

---
+++ ForLoop.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+int main()
+ {
+ 	
+// 	Even Number
+
+//int i;
+//for(i=2;i<=10;i=i+2)
+//{
+//	printf("%d\n",i);
+//}
+
+
+//odd number
+
+//int i;
+//for(i=1;i<=10;i=i+2)
+//{
+//	printf("%d\n",i);
+//}
+
+
+//Ask from the User for Numbers he wants to print
+
+//int num, i;
+//
+//printf("Enter a Number");
+//scanf("%d",&num);
+//for(i=1;i<=num;i++)
+//{
+//	printf("%d\n",i);
+//}
+
+
+//Addition for all integer number input from users
+
+//int num, i,sum=0;
+//
+//printf("Enter a Number");
+//scanf("%d",&num);
+//
+//for(i=1;i<=num;i++)
+//{
+//sum=sum+i;
+//}
+//printf("sum=%d\n",sum);
+
+
+
+
+//Even and Odd Sum
+//
+//int num, evensum=0, oddsum=;
+//
+//printf("Enter a Number");
+//scanf("%d",&num);
+//
+//for(i=1;i<=num;i++)
+//{
+//sum=sum+i;
+//}
+//printf("sum=%d\n",sum);
+
+
+
+  return 0;
+}
+
