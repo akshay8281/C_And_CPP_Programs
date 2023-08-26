d8a00cfc29c281789cc9f079c66efc20eb4b0836
akshay8281 <akshaypitroda4@gmail.com>
2023-08-26 21:57:10 +0530

    StringFirst

---
+++ STRING_First.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+ {
+ 	
+// 	Strings
+
+char varname[20]="Akshay";
+ printf("%s",varname);
+printf("%c",varname[0]);
+printf("%c",varname[3]);
+
+  return 0;
+}
